3a0469c466f543567cae7207edeb9b906c5edf87
root <root@Ram.localdomain>
2020-07-06 07:34:50 +0530

    Initial commit

---
+++ 001-Basics/PrintName.swift
@@ -0,0 +1,11 @@
+class PrintName {
+    var name = "rikhi"
+    var email = "ram@"
+    var mobile = 99759       
+}
+let namePrint = PrintName()
+namePrint.name = "Rikhi"
+namePrint.email = "ram@gmail.com"
+namePrint.mobile = 98978
+print("Name:"+namePrint.name+"\nEmail:"+namePrint.email+"\nMobile:"+namePrint.mobile)
+
---
+++ 002-Data-Types/Integer.swift
@@ -0,0 +1,13 @@
+class Person {
+  var id:Int = 101
+  var name:String = "Rikhi Ram Satnami"
+  var email:String = "rikhi@gmail.com"
+  var mobile:Int = 9040652072
+    
+}
+
+let person = Person()
+print("Id     :: \(person.id)")
+print("Name   :: \(person.name)")
+print("Email  :: \(person.email)")
+print("Mobile :: \(person.mobile)")
